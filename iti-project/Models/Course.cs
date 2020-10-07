6b58c7fae1a2d53cdfff23a91dbc53dd5a1c3bba
AhmedMojahed <asaad0861@gmail.com>
2020-10-07 16:58:20 +0200

    Added Model Classes for db

---
+++ iti-project/Models/Course.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iti_project.Models
+{
+    public class Course
+    {
+        public Course()
+        {
+            this.Users = new List<ApplicationUser>();
+        }
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Course Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Department Name")]
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+        public IList<ApplicationUser> Users { get; set; }
+    }
+}
---
+++ iti-project/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace iti_project.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Department Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+}
